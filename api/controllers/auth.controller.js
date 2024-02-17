cbf317c439f9160397b0b89366ff131982d10e20
Agash <agash@mail.com>
2024-02-17 15:41:46 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from "../models/user.model.js";
 import brcypt from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import Jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-        next(errorHandler(400, 'All fields are required'));
+        return next(errorHandler(400, 'All fields are required'));
     }
 
     const hashPassword = brcypt.hashSync(password, 10);
@@ -20,6 +21,36 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.json({ message: 'Sign Up Successful' })
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(errorHandler(400, 'Email/Password is empty'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(400, 'User not found'));
+        }
+        const validPassword = brcypt.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Password is invalid'));
+        }
+
+        const token = Jwt.sign(
+            { userId: validUser._id },
+            process.env.JWT_SECRET_KEY
+        );
+        const {password: pass, ...rest} = validUser._doc;
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
     } catch (err) {
         next(err);
     }
