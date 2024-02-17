0bfa54b3c7fdbf296d6427d4371e39514b58adad
Agash <agash@mail.com>
2024-02-17 11:56:39 +0530

    mongoose connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
+mongoose
+    .connect(process.env.MONG_URL)
+    .then(console.log('mongo db connected'))
+    .catch((err) => {
+        console.error(err);
+    })
 
 const app = express();
 
