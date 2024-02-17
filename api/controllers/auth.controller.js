54c3e2c2fc08ac584228189eff457d83f8e1e87b
Agash <agash@mail.com>
2024-02-17 13:48:54 +0530

    create a middleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 import User from "../models/user.model.js";
 import brcypt from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-    console.log(req.body);
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-        return res.status(400).json({ message: 'All fields are required!' });
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashPassword = brcypt.hashSync(password, 10);
@@ -21,6 +21,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json({ message: 'Sign Up Successful' })
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        next(err);
     }
 }
