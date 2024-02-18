3f9edd7a811fd48cc909699237a046967863ef5e
Agash <agash@mail.com>
2024-02-18 23:55:35 +0530

    delete user routes

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -25,12 +25,12 @@ export const updateUser = async (req, res, next) => {
             }
         }
         try {
-            const updatedUser = await User.findOneAndUpdate({_id: req.params.userId}, {
+            const updatedUser = await User.findOneAndUpdate({ _id: req.params.userId }, {
                 username: req.body.username,
                 email: req.body.email,
                 password: req.body.password,
                 profilePicture: req.body.profilePicture
-            }, {new: true});
+            }, { new: true });
             const { password, ...rest } = updatedUser._doc
             res.status(200).json(rest);
         } catch (err) {
@@ -38,5 +38,17 @@ export const updateUser = async (req, res, next) => {
         }
     } else {
         return next(errorHandler(400, 'Password required'))
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.userId !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to delete this user'))
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User Deleted');
+    } catch (error) {
+        next(error);
     }
 }
