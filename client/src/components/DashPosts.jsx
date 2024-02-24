1e0c9a6747efa278dee6bd37024713bb5072ea79
Agash <agash@mail.com>
2024-02-24 09:59:07 +0530

    need to fix postslice

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,19 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPosts } from "../redux/post/postSlice";
+import { fetchPosts, selectAllPosts } from "../redux/post/postSlice";
 import { Link } from 'react-router-dom';
 import { Table } from 'flowbite-react'
 
 const DashPosts = () => {
+  const [showMore, setShowMore] = useState(true);
   const { currentUser } = useSelector(state => state.user);
   const dispatch = useDispatch();
+
+  // const postState = useSelector(state => state.post);
+  // console.log(postState);
+  // setUserPosts(postState.post.posts);
+  // console.log(userPosts);
+  // if (userPosts && userPosts.length < 9) {
+  //   const startIndex = userPosts.length;
+  //   ()=>setShowMore(false);
+  //   console.log(showMore);
+  //   // dispatch(fetchPosts({ id: currentUser._id, startIndex: startIndex })).unwrap();
+
+  // // const postState = useSelector(state => state.post);
+  // // console.log(postState);
+  // }
+  const handleShowMore = async () => {
+    // const startIndex = userPosts.length;
+    // const a = dispatch(fetchPosts({ id: currentUser._id, startIndex })).unwrap();
+    // console.log(a);
+
+  }
   useEffect(() => {
     if (currentUser.isAdmin) {
-      dispatch(fetchPosts({ id: currentUser._id })).unwrap();
+      dispatch(fetchPosts({ id: currentUser._id, startIndex: 0 })).unwrap();
     }
-  }, [currentUser._id]);
-  const { posts: userPosts, lastMonthPosts, totalPosts } = useSelector(state => state.post);
-  console.log(userPosts, lastMonthPosts, totalPosts);
+  }, [currentUser, dispatch]);
+  try {
+    const userPosts = useSelector(selectAllPosts) || [];
+    console.log('userPosts:', userPosts);
+  } catch (error) {
+    console.error('Error in useSelector:', error);
+  }
+  // console.log(userPosts);
+  const userPosts=[];
+
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-100 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -28,7 +56,7 @@ const DashPosts = () => {
               <Table.HeadCell>Delete</Table.HeadCell>
             </Table.Head>
             <Table.Body className="divide-y">
-              {userPosts.map(post => (
+              {userPosts?.map(post => (
                 <Table.Row key={post.slug} className="bg-white dark:border-gray-700 dark:bg-gray-800">
                   <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
@@ -49,8 +77,12 @@ const DashPosts = () => {
                 </Table.Row>
               ))}
             </Table.Body>
-
           </Table>
+          {
+            showMore && (
+              <button type='button' onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show More</button>
+            )
+          }
         </>
       ) : (
         <p>You Have No Posts</p>
--- client/src/redux/post/postSlice.js
+++ client/src/redux/post/postSlice.js
@@ -1,20 +1,20 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit"
 // import axios from "axios";
-
-const initialState = {
+const postsAdapter = createEntityAdapter()
+const initialState = postsAdapter.getInitialState({
     state: 'idle',
     error: null,
     lastMonthPosts: 0,
     totalPosts: 0,
     posts: null,
     loading: false,
-}
+});
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async (params) => {
     const { id } = params;
     // const res = await axios.get(`/api/post/getposts?userId=${id}`);
     try {
-        const res = await fetch(`/api/post/getposts?userId=${id}`, {
+        const res = await fetch(`/api/post/getposts?userId=${id}&startIndex=9`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -44,10 +44,16 @@ const postSlice = createSlice({
                 state.status = 'loading'
             })
             .addCase(fetchPosts.fulfilled, (state, action) => {
-                state.posts = action.payload.posts;
-                state.lastMonthPosts = action.payload.lastMonthPosts;
-                state.totalPosts = action.payload.totalPosts;
+                // state.posts = action.payload.posts;
+                // state.lastMonthPosts = action.payload.lastMonthPosts;
+                // state.totalPosts = action.payload.totalPosts;
                 state.status = 'success';
+                action.payload.posts.map(post => {
+                    post.id = post._id
+                    return post;
+                })
+                console.log(action.payload.posts)
+                postsAdapter.upsertMany(state, action.payload.posts);
             })
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'fail'
@@ -55,6 +61,8 @@ const postSlice = createSlice({
             })
     }
 })
-
+export const {
+    selectAll: selectAllPosts
+} = postsAdapter.getSelectors((state) => state.post)
 export const { postsFetchStart, postsFetchEnd } = postSlice.actions;
 export default postSlice.reducer;
